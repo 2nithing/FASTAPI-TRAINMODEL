44f089c93a70f3e88126c319e27c3b09a0833f1d
Nithin G <79520840+2nithing@users.noreply.github.com>
2024-08-13 15:12:59 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,UploadFile
 from typing import List
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.saving import load_model
 from PIL import Image
 import io
 import numpy as np
@@ -26,6 +27,23 @@ async def train(files:List[UploadFile]):
             label = await file.read()                
             label = re.findall(r'\d+', str(label))
     if data:
-        model = training.train(data,label)      
-        print("model trained")
-    return {'result':np.array(data).shape,'labels':label}
+        model = training.train(data,label)    
+        model.save('model.keras')
+    return {'result':'model trained'}
+
+@app.post('/test')
+async def testing(file:UploadFile):
+    img = await file.read()
+    img = Image.open(io.BytesIO(img))
+    img = img.resize((160,160))
+    img = np.array(img)
+    img = img/255
+    img = img.reshape(1,160,160,3)
+    model = load_model('model.keras')
+    result = model.predict(img)
+    print (result)
+    if np.argmax(result)==0:
+        response='cat'
+    elif np.argmax(result)==1:
+        response = 'dog'
+    return {'result':response}
